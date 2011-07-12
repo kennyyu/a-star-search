d35e84f7546313c52c392c8cd85e5578dfa0884b
Kenny Yu <yu.kenny124@gmail.com>
2011-07-12 00:25:06 -0400

    Added comments to header files

--- lib/src/queue.h
+++ lib/src/queue.h
@@ -1,11 +1,15 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+/* 
+ * Alias queue to be a pointer to a struct queue.
+ */
 typedef struct queue *queue;
 
-queue queue_create();
-void queue_free(queue);
-
+/*
+ * Enumeration of error and success return values for functions that return an
+ * int. 
+ */
 typedef enum queue_error_numbers 
 {
 	SUCCESS_QUEUE = 1,
@@ -15,11 +19,52 @@ typedef enum queue_error_numbers
 	ERROR_QUEUE_MALLOC_FAIL = -4
 } queue_error_numbers;
 
+/* 
+ * Initialize a queue and returns a pointer to the queue. If there is not
+ * enough memory, this returns NULL.
+ */
+queue queue_create();
+
+/* 
+ * Frees the memory held by the queue.
+ */
+void queue_free(queue);
+
+/* 
+ * Returns the length of the queue. If the queue is NULL, this returns 
+ * ERROR_QUEUE_IS_NULL.
+ */
 int queue_size(queue);
+
+/* 
+ * Returns 1 if the queue is empty, otherwise returns 0. If the queue is NULL,
+ * this returns ERROR_QUEUE_IS_NULL.
+ */
 int queue_is_empty(queue);
+
+/* 
+ * Returns 1 if the item is in the queue, otherwise returns 0. If the queue is
+ * NULL, this returns ERROR_QUEUE_IS_NULL. If the item is NULL, this returns
+ * ERROR_QUEUE_ITEM_IS_NULL.
+ */
 int queue_contains(queue, void *);
+
+/* 
+ * Returns a pointer to the top item in the queue. If the queue is NULL
+ * or is empty, this returns NULL.
+ */
 void *queue_peek(queue);
+
+/* 
+ * Adds the item to the queue. If successful, this returns SUCCESS_QUEUE. If 
+ * the queue is NULL, this returns ERROR_QUEUE_IS_NULL.
+ */
 int queue_enqueue(queue, void *);
+
+/* 
+ * Removes the first item in the queue and returns a pointer to it. If the 
+ * item is not found or if the queue is empty or NULL, this returns NULL.
+ */
 void *queue_dequeue(queue);
 
 #endif
--- lib/src/stack.h
+++ lib/src/stack.h
@@ -1,11 +1,15 @@
 #ifndef STACK_H
 #define STACK_H
 
+/* 
+ * Alias stack to be a pointer to a struct stack.
+ */
 typedef struct stack *stack;
 
-stack stack_create();
-void stack_free(stack);
-
+/*
+ * Enumeration of error and success return values for functions that return an
+ * int. 
+ */
 typedef enum stack_error_numbers 
 {
 	SUCCESS_STACK = 1,
@@ -15,11 +19,52 @@ typedef enum stack_error_numbers
 	ERROR_STACK_MALLOC_FAIL = -4
 } stack_error_numbers;
 
+/* 
+ * Initialize a stack and returns a pointer to the stack. If there is not
+ * enough memory, this returns NULL.
+ */
+stack stack_create();
+
+/* 
+ * Frees the memory held by the stack.
+ */
+void stack_free(stack);
+
+/* 
+ * Returns the length of the stack. If the stackis NULL, this returns 
+ * ERROR_STACK_IS_NULL.
+ */
 int stack_size(stack);
+
+/* 
+ * Returns 1 if the stack is empty, otherwise returns 0. If the stack is NULL,
+ * this returns ERROR_STACK_IS_NULL.
+ */
 int stack_is_empty(stack);
+
+/* 
+ * Returns 1 if the item is in the stack, otherwise returns 0. If the stack is
+ * NULL, this returns ERROR_STACK_IS_NULL. If the item is NULL, this returns
+ * ERROR_STACK_ITEM_IS_NULL.
+ */
 int stack_contains(stack, void *);
+
+/* 
+ * Returns a pointer to the top item in the stack. If the stack is NULL
+ * or is empty, this returns NULL.
+ */
 void *stack_peek(stack);
+
+/* 
+ * Adds the item to the stack. If successful, this returns SUCCESS_STACK. If 
+ * the stack is NULL, this returns ERROR_STACK_IS_NULL.
+ */
 int stack_push(stack, void *);
+
+/* 
+ * Removes the top item from the stack and returns a pointer to it. If the 
+ * item is not found or if the stack is empty or NULL, this returns NULL.
+ */
 void *stack_pop(stack);
 
 #endif
