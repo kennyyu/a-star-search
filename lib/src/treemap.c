482a72105a2ea45f499e2f07ff0f2e91fcc3def8
Kenny Yu <yu.kenny124@gmail.com>
2011-07-30 02:55:16 -0400

    formatting for .gitignore, tabs->spaces

--- lib/src/treemap.c
+++ lib/src/treemap.c
@@ -2,166 +2,166 @@
 #include <stdlib.h>
 #include "treemap.h"
 
-typedef struct __binarytree_node *__binarytree_node;	
+typedef struct __binarytree_node *__binarytree_node;  
 struct __binarytree_node {
-	map_node data;
-	__binarytree_node left;
-	__binarytree_node right;
+  map_node data;
+  __binarytree_node left;
+  __binarytree_node right;
 };
 
 typedef struct _treemap *_treemap;
 struct _treemap {
-	int size;
-	map_compare compare;
-	__binarytree_node root;
+  int size;
+  map_compare compare;
+  __binarytree_node root;
 };
 
 map _treemap_create(map_compare cmp, map_hash hash, map_equal eq) {
-	if (!cmp)
-		return NULL;
-	if (hash || eq)
-		return NULL;
-	_treemap mp = malloc(sizeof(struct _treemap));
-	if (!mp)
-		return NULL;
-	mp->size = 0;
-	mp->compare = cmp;
-	mp->root = NULL;
-	return (map) mp;
+  if (!cmp)
+    return NULL;
+  if (hash || eq)
+    return NULL;
+  _treemap mp = malloc(sizeof(struct _treemap));
+  if (!mp)
+    return NULL;
+  mp->size = 0;
+  mp->compare = cmp;
+  mp->root = NULL;
+  return (map) mp;
 }
 
 void __treemap_free_helper(__binarytree_node node) {
-	if (!node)
-		return;
-	__treemap_free_helper(node->left);
-	__treemap_free_helper(node->right);
+  if (!node)
+    return;
+  __treemap_free_helper(node->left);
+  __treemap_free_helper(node->right);
         free(node->data);
-	free(node);
+  free(node);
 }
 
 void _treemap_free(map mapp) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return;
-	__treemap_free_helper(mp->root);
-	free(mp);
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return;
+  __treemap_free_helper(mp->root);
+  free(mp);
 }
 
 int _treemap_size(map mapp) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return ERROR_MAP_IS_NULL;
-	return mp->size;
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return ERROR_MAP_IS_NULL;
+  return mp->size;
 }
 
 int _treemap_is_empty(map mapp) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return ERROR_MAP_IS_NULL;
-	return _treemap_size((map) mp) == 0;
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return ERROR_MAP_IS_NULL;
+  return _treemap_size((map) mp) == 0;
 }
 
 int __treemap_contains_helper(map_compare cmp, __binarytree_node node, void *key) {
-	if (!node)
-		return 0;
-	if (cmp(node->data->key, key) == 0) {
-		return 1;
-	}
-	return __treemap_contains_helper(cmp, node->left, key) || 
-		__treemap_contains_helper(cmp, node->right, key);
+  if (!node)
+    return 0;
+  if (cmp(node->data->key, key) == 0) {
+    return 1;
+  }
+  return __treemap_contains_helper(cmp, node->left, key) || 
+    __treemap_contains_helper(cmp, node->right, key);
 }
 
 int _treemap_contains(map mapp, void *key) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return ERROR_MAP_IS_NULL;
-	if (!key)
-		return ERROR_MAP_ITEM_IS_NULL;
-	return __treemap_contains_helper(mp->compare, mp->root, key);
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return ERROR_MAP_IS_NULL;
+  if (!key)
+    return ERROR_MAP_ITEM_IS_NULL;
+  return __treemap_contains_helper(mp->compare, mp->root, key);
 }
 
 /* in-order traversal (left, this, right) */
 int __treemap_keys_to_array_helper(void **keys, __binarytree_node node, int position) {
-	if (!keys)
-		return 0;
-	if (!node)
-		return position;
-	int new_position = __treemap_keys_to_array_helper(keys, node->left, position);
-	keys[new_position] = node->data->key;
-	return __treemap_keys_to_array_helper(keys, node->right, new_position + 1);
+  if (!keys)
+    return 0;
+  if (!node)
+    return position;
+  int new_position = __treemap_keys_to_array_helper(keys, node->left, position);
+  keys[new_position] = node->data->key;
+  return __treemap_keys_to_array_helper(keys, node->right, new_position + 1);
 }
 
 void **_treemap_keys_to_array(map mapp) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return NULL;
-	if (!mp->size)
-		return NULL;
-	void **keys = malloc(sizeof(void *) * mp->size);
-	if (!keys)
-		return NULL;
-	int num_keys_added = __treemap_keys_to_array_helper(keys, mp->root, 0);
-	
-	/* make sure nothing went wrong while adding items to array */
-	if (num_keys_added != mp->size) {
-		free(keys);
-		return NULL;
-	}
-	return keys;
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return NULL;
+  if (!mp->size)
+    return NULL;
+  void **keys = malloc(sizeof(void *) * mp->size);
+  if (!keys)
+    return NULL;
+  int num_keys_added = __treemap_keys_to_array_helper(keys, mp->root, 0);
+  
+  /* make sure nothing went wrong while adding items to array */
+  if (num_keys_added != mp->size) {
+    free(keys);
+    return NULL;
+  }
+  return keys;
 }
 
 /* in-order traversal (left, this, right) */
 int __treemap_to_array_helper(map_node *nodes, __binarytree_node node, int position) {
-	if (!nodes)
-		return 0;
-	if (!node)
-		return position;
-	int new_position = __treemap_to_array_helper(nodes, node->left, position);
-	nodes[new_position] = node->data;
-	return __treemap_to_array_helper(nodes, node->right, new_position + 1);
+  if (!nodes)
+    return 0;
+  if (!node)
+    return position;
+  int new_position = __treemap_to_array_helper(nodes, node->left, position);
+  nodes[new_position] = node->data;
+  return __treemap_to_array_helper(nodes, node->right, new_position + 1);
 }
 
 map_node *_treemap_to_array(map mapp) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return NULL;
-	if (!mp->size)
-		return NULL;
-	map_node *nodes = malloc(sizeof(map_node) * mp->size);
-	if (!nodes)
-		return NULL;
-	int num_nodes_added = __treemap_to_array_helper(nodes, mp->root, 0);
-	
-	/* make sure nothing went wrong while adding items to array */
-	if (num_nodes_added != mp->size) {
-		free(nodes);
-		return NULL;
-	}
-	return nodes;
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return NULL;
+  if (!mp->size)
+    return NULL;
+  map_node *nodes = malloc(sizeof(map_node) * mp->size);
+  if (!nodes)
+    return NULL;
+  int num_nodes_added = __treemap_to_array_helper(nodes, mp->root, 0);
+  
+  /* make sure nothing went wrong while adding items to array */
+  if (num_nodes_added != mp->size) {
+    free(nodes);
+    return NULL;
+  }
+  return nodes;
 }
 
 int _treemap_add(map mapp, void *key, void *value) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return ERROR_MAP_IS_NULL;
-	if (!key)
-		return ERROR_MAP_ITEM_IS_NULL;
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return ERROR_MAP_IS_NULL;
+  if (!key)
+    return ERROR_MAP_ITEM_IS_NULL;
         
   /* Check if the root is NULL */
-	__binarytree_node current = mp->root;
+  __binarytree_node current = mp->root;
   if (!current) {
     current = malloc(sizeof(struct __binarytree_node));
-		if (!current)
-			return ERROR_MAP_MALLOC_FAIL;
-		current->data = malloc(sizeof(struct map_node));
-		if (!current->data) {
+    if (!current)
+      return ERROR_MAP_MALLOC_FAIL;
+    current->data = malloc(sizeof(struct map_node));
+    if (!current->data) {
       free(current);
-			return ERROR_MAP_MALLOC_FAIL;
-		}
+      return ERROR_MAP_MALLOC_FAIL;
+    }
     current->data->key = key;
     current->data->value = value;
-		current->left = NULL;
-		current->right = NULL;
+    current->left = NULL;
+    current->right = NULL;
     mp->root = current;
     mp->size++;
     return SUCCESS_MAP;
@@ -169,63 +169,63 @@ int _treemap_add(map mapp, void *key, void *value) {
   
   /* descend the tree with tailing pointers */
   __binarytree_node previous = NULL;
-	map_compare cmp = mp->compare;
-	int comparison;
-	while (current) {
-		comparison = cmp(key, current->data->key);
-		if (comparison < 0) {
+  map_compare cmp = mp->compare;
+  int comparison;
+  while (current) {
+    comparison = cmp(key, current->data->key);
+    if (comparison < 0) {
       previous = current;
-			current = current->left;
-		} else if (comparison > 0) {
+      current = current->left;
+    } else if (comparison > 0) {
       previous = current;
-			current = current->right;
-		} else {
+      current = current->right;
+    } else {
       /* key found, update value */
-			current->data->value = value;
+      current->data->value = value;
       return SUCCESS_MAP;
     }  
-	}
+  }
         
   /* update previous to point to the new node */
   current = malloc(sizeof(struct __binarytree_node));
   if (!current)
-		return ERROR_MAP_MALLOC_FAIL;
+    return ERROR_MAP_MALLOC_FAIL;
   current->data = malloc(sizeof(struct map_node));
   if (!current->data) {
-		free(current);
-		return ERROR_MAP_MALLOC_FAIL;
+    free(current);
+    return ERROR_MAP_MALLOC_FAIL;
   }
   current->data->key = key;
   current->data->value = value;
-	current->left = NULL;
-	current->right = NULL;
+  current->left = NULL;
+  current->right = NULL;
   if (comparison < 0)
-		previous->left = current;
+    previous->left = current;
   else
-		previous->right = current;
+    previous->right = current;
   mp->size++;
   return SUCCESS_MAP;
 }
 
 void *_treemap_get(map mapp, void *key) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return NULL;
-	if (!key)
-		return NULL;
-	__binarytree_node current = mp->root;
-	map_compare cmp = mp->compare;
-	int comparison;
-	while (current) {
-		comparison = cmp(key, current->data->key);
-		if (comparison < 0)
-			current = current->left;
-		else if (comparison > 0)
-			current = current->right;
-		else
-			return current->data->value;
-	}
-	return NULL;
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return NULL;
+  if (!key)
+    return NULL;
+  __binarytree_node current = mp->root;
+  map_compare cmp = mp->compare;
+  int comparison;
+  while (current) {
+    comparison = cmp(key, current->data->key);
+    if (comparison < 0)
+      current = current->left;
+    else if (comparison > 0)
+      current = current->right;
+    else
+      return current->data->value;
+  }
+  return NULL;
 }
 
 __binarytree_node __treemap_remove_least(map_compare cmp, __binarytree_node node, map_node least) {
@@ -236,11 +236,11 @@ __binarytree_node __treemap_remove_least(map_compare cmp, __binarytree_node node
   __binarytree_node previousprevious = NULL;
   __binarytree_node previous = NULL;
   __binarytree_node current = node;
-	while (current) {
+  while (current) {
     previousprevious = previous;
     previous = current;
     current = current->left;
-	}
+  }
   /* current is NULL, previous is least */
   least->key = previous->data->key;
   least->value = previous->data->value;
@@ -254,34 +254,34 @@ __binarytree_node __treemap_remove_least(map_compare cmp, __binarytree_node node
     previousprevious->left = previous->right;
     free(previous->data);
     free(previous);
-  	return node;
+    return node;
   }
 }
 
 void *_treemap_remove(map mapp, void *key) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return NULL;
-	if (!key)
-		return NULL;
-	__binarytree_node current = mp->root;
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return NULL;
+  if (!key)
+    return NULL;
+  __binarytree_node current = mp->root;
   if (current == NULL)
-		return NULL;
+    return NULL;
 
   __binarytree_node previous = NULL;
-	map_compare cmp = mp->compare;
-	int comparison;
-	while (current) {
-		comparison = cmp(key, current->data->key);
-		if (comparison < 0) {
+  map_compare cmp = mp->compare;
+  int comparison;
+  while (current) {
+    comparison = cmp(key, current->data->key);
+    if (comparison < 0) {
       previous = current;
-			current = current->left;
-		}
-		else if (comparison > 0) {
+      current = current->left;
+    }
+    else if (comparison > 0) {
       previous = current;
-			current = current->right;
-		}
-		else {
+      current = current->right;
+    }
+    else {
       /* remove least item from the right subtree of current
        * and store the least node in the address of least.
        * remove_least will return the new root after the removal */
@@ -293,15 +293,15 @@ void *_treemap_remove(map mapp, void *key) {
       if (!new_root && !least->key) {
         /* there was nothing in the right subtree
          * replace current with left subtree */
-				if (current == mp->root) {
-					/* remove root of tree */
-					mp->root = current->left;
-				} else {
-	        if (previous->left == current)
-	          previous->left = current->left;
-	        else
-	          previous->right = current->left;
-				}
+        if (current == mp->root) {
+          /* remove root of tree */
+          mp->root = current->left;
+        } else {
+          if (previous->left == current)
+            previous->left = current->left;
+          else
+            previous->right = current->left;
+        }
       } else {
         /* set least's right to the remaining right subtree
          * set least's left to current's left subtree
@@ -312,54 +312,54 @@ void *_treemap_remove(map mapp, void *key) {
         new_current->data = least;
         new_current->left = current->left;
         new_current->right = new_root;
-				if (current == mp->root) {
-					/* remove root of tree */
-					mp->root = new_current;
-				} else {
-	        if (previous->left == current)
-	          previous->left = new_current;
-	        else
-	          previous->right = new_current;
-				}
+        if (current == mp->root) {
+          /* remove root of tree */
+          mp->root = new_current;
+        } else {
+          if (previous->left == current)
+            previous->left = new_current;
+          else
+            previous->right = new_current;
+        }
       }
       void *value = current->data->value;
       free(current->data);
       free(current);
       mp->size--;
       return value;
-		}
-	}
+    }
+  }
   /* item not found */
   return NULL;
 }
 
 map_node _treemap_remove_random(map mapp) {
-	_treemap mp = (_treemap) mapp;
-	if (!mp)
-		return NULL;
-	if (!mp->root)
-		return NULL;
-		
-	map_node least = malloc(sizeof(struct map_node));
+  _treemap mp = (_treemap) mapp;
+  if (!mp)
+    return NULL;
+  if (!mp->root)
+    return NULL;
+    
+  map_node least = malloc(sizeof(struct map_node));
   least->key = NULL;
   __binarytree_node new_root = __treemap_remove_least(mp->compare, mp->root, least);
-	if (!least->key)
-		return NULL;
-	mp->root = new_root;
-	mp->size--;
-	return least;
+  if (!least->key)
+    return NULL;
+  mp->root = new_root;
+  mp->size--;
+  return least;
 }
 
 map_methods treemap_methods = {
-	.create = &_treemap_create,
-	.free = &_treemap_free,
-	.size = &_treemap_size,
-	.is_empty = &_treemap_is_empty,
-	.contains = &_treemap_contains,
+  .create = &_treemap_create,
+  .free = &_treemap_free,
+  .size = &_treemap_size,
+  .is_empty = &_treemap_is_empty,
+  .contains = &_treemap_contains,
   .to_array = &_treemap_to_array,
-	.keys_to_array = &_treemap_keys_to_array,
-	.add = &_treemap_add,
-	.get = &_treemap_get,
-	.remove = &_treemap_remove,
-	.remove_random = &_treemap_remove_random,
+  .keys_to_array = &_treemap_keys_to_array,
+  .add = &_treemap_add,
+  .get = &_treemap_get,
+  .remove = &_treemap_remove,
+  .remove_random = &_treemap_remove_random,
 };
--- lib/src/treeset.c
+++ lib/src/treeset.c
@@ -6,90 +6,90 @@ typedef struct _treeset *_treeset;
 struct _treeset { };
 
 set _treeset_create(set_compare cmp, set_hash hash, set_equal eq) {
-	if (!cmp)
-		return NULL;
-	if (hash || eq)
-		return NULL;
-	map se = treemap_methods.create(cmp, hash, eq);
-	if (!se)
-		return NULL;
-	return (set) se;
+  if (!cmp)
+    return NULL;
+  if (hash || eq)
+    return NULL;
+  map se = treemap_methods.create(cmp, hash, eq);
+  if (!se)
+    return NULL;
+  return (set) se;
 }
 
 void _treeset_free(set se) {
-	if (!se)
-		return;
-	treemap_methods.free((map) se);
+  if (!se)
+    return;
+  treemap_methods.free((map) se);
 }
 
 int _treeset_size(set se) {
-	if (!se)
-		return ERROR_SET_IS_NULL;
-	return treemap_methods.size((map) se);
+  if (!se)
+    return ERROR_SET_IS_NULL;
+  return treemap_methods.size((map) se);
 }
 
 int _treeset_is_empty(set se) {
-	if (!se)
-		return ERROR_SET_IS_NULL;
-	return treemap_methods.is_empty((map) se);
+  if (!se)
+    return ERROR_SET_IS_NULL;
+  return treemap_methods.is_empty((map) se);
 }
 
 int _treeset_contains(set se, void *item) {
-	if (!se)
-		return ERROR_SET_IS_NULL;
-	if (!item)
-		return ERROR_SET_ITEM_IS_NULL;
-	return treemap_methods.contains((map) se, item);
+  if (!se)
+    return ERROR_SET_IS_NULL;
+  if (!item)
+    return ERROR_SET_ITEM_IS_NULL;
+  return treemap_methods.contains((map) se, item);
 }
 
 void **_treeset_to_array(set se) {
-	if (!se)
-		return NULL;
-	return treemap_methods.keys_to_array((map) se);
+  if (!se)
+    return NULL;
+  return treemap_methods.keys_to_array((map) se);
 }
 
 int _treeset_add(set se, void *item) {
-	if (!se)
-		return ERROR_SET_IS_NULL;
-	if (!item)
-		return ERROR_SET_ITEM_IS_NULL;
-	int error = treemap_methods.add((map) se, item, NULL);
-	if (error == ERROR_MAP_MALLOC_FAIL)
-		return ERROR_SET_MALLOC_FAIL;
-	return SUCCESS_SET;
+  if (!se)
+    return ERROR_SET_IS_NULL;
+  if (!item)
+    return ERROR_SET_ITEM_IS_NULL;
+  int error = treemap_methods.add((map) se, item, NULL);
+  if (error == ERROR_MAP_MALLOC_FAIL)
+    return ERROR_SET_MALLOC_FAIL;
+  return SUCCESS_SET;
 }
 
 int _treeset_remove(set se, void *item) {
-	if (!se)
-		return ERROR_SET_IS_NULL;
-	if (!item)
-		return ERROR_SET_ITEM_IS_NULL;
-	if (!treemap_methods.size((map) se))
-		return ERROR_SET_IS_EMPTY;
-	if (!treemap_methods.contains((map) se, item))
-		return ERROR_SET_ITEM_NOT_FOUND;
-	treemap_methods.remove((map) se, item);
-	return SUCCESS_SET;
+  if (!se)
+    return ERROR_SET_IS_NULL;
+  if (!item)
+    return ERROR_SET_ITEM_IS_NULL;
+  if (!treemap_methods.size((map) se))
+    return ERROR_SET_IS_EMPTY;
+  if (!treemap_methods.contains((map) se, item))
+    return ERROR_SET_ITEM_NOT_FOUND;
+  treemap_methods.remove((map) se, item);
+  return SUCCESS_SET;
 }
 
 /* this will remove the minimum */
 void *_treeset_remove_random(set se) {
-	if (!se)
-		return NULL;
-	map_node node = treemap_methods.remove_random((map) se);
-	if (!node)
-		return NULL;
-	return node->key;
+  if (!se)
+    return NULL;
+  map_node node = treemap_methods.remove_random((map) se);
+  if (!node)
+    return NULL;
+  return node->key;
 }
 
 set_methods treeset_methods = {
-	.create = &_treeset_create,
-	.free = &_treeset_free,
-	.size = &_treeset_size,
-	.is_empty = &_treeset_is_empty,
-	.contains = &_treeset_contains,
-	.to_array = &_treeset_to_array,
-	.add = &_treeset_add,
-	.remove = &_treeset_remove,
-	.remove_random = &_treeset_remove_random,
+  .create = &_treeset_create,
+  .free = &_treeset_free,
+  .size = &_treeset_size,
+  .is_empty = &_treeset_is_empty,
+  .contains = &_treeset_contains,
+  .to_array = &_treeset_to_array,
+  .add = &_treeset_add,
+  .remove = &_treeset_remove,
+  .remove_random = &_treeset_remove_random,
 };
